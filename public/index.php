8e7d1c745d7405a79f9de12f356c66953854c9f0
yulia633 <ignat633yulia@yandex.ru>
2021-03-28 22:52:28 +0300

    used template

--- public/index.php
+++ public/index.php
@@ -4,29 +4,39 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+// Список пользователей
+// Каждый пользователь – ассоциативный массив
+// следующей структуры: id, firstName, lastName, email
+$users = App\Generator::generate(100);
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+$app->get('/users', function ($request, $response) use ($users) {
+    $params = [
+        'users' => $users
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
-
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
-
-$app->get('/courses/{id}', function ($request, $response, array $args) {
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
     $id = $args['id'];
-    return $response->write("Course id: {$id}");
+    $user = collect($users)->firstWhere('id', $id);
+    $params = ['user' => $user];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
