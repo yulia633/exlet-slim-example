28dd06411fd0d60c067f6b8ed7ece1ae5d411aff
yulia633 <ignat633yulia@yandex.ru>
2021-04-08 18:29:10 +0300

    refactor

--- public/index.php
+++ public/index.php
@@ -37,18 +37,57 @@ $app->get('/', function ($request, $response) {
 })->setName('index');
 
 $app->get('/users', function ($request, $response) {
+    
 
-    $repo = new Repository();
-    $allUsers = $repo->all();
+    //$repo = new Repository();
+    //$json = $repo->all('users'); //all
+
+    //$users = $request->getParsedBodyParam('user');
+    $json = new Repository('users');
+
+    $allUsers = $json->select();//get arrayall
+    // $users = $json->select();
+   
+    // $users = [];
+
+    // foreach ($allUsers as $key => $value) {
+    //    // var_dump($value) ;
+    //     if (!array_key_exists($value, $users)) {
+    //         $users[$key][] = $allUsers[$key];
+    //     }
+       
+    // }
+
+    // var_dump($users);die;
+
+
+
+    //$users = $json->readFile();
+  // var_dump($json->select(["id" => $id]));die;
+
+    // $page = $request->getQueryParam('page', 1);
+    // $per = $request->getQueryParam('per', 5);
+    // $offset = ($page - 1) * $per;
+
+    // $sliceUsers = [];
+    // foreach ($allUsers as $user) {
+    //     $sliceUsers[] = array_slice($user, $offset, $per);
+    // }
+
+ //  var_dump($allUsers);die;
+    
 
     $flash = $this->get('flash')->getMessages();
     $page = $request->getQueryParam('page', 1);
     $per = $request->getQueryParam('per', 5);
     $offset = ($page - 1) * $per;
-    $sliceUsers = array_slice($allUsers, $offset, $per);
 
-    $search = $request->getQueryParam('search', null);
-    $filteredUsers = array_filter($allUsers, fn ($user) => is_numeric(strpos($user->nickname, $search)));
+
+   $sliceUsers = array_slice($allUsers, $offset, $per);
+   // var_dump($sliceUsers);die;
+
+    $search = $request->getQueryParam('search');
+    $filteredUsers = array_filter($allUsers, fn ($user) => is_numeric(strpos($user['nickname'] ?? " ", $search)));
     $users = $search ? $filteredUsers : $sliceUsers;
 
     $params = [
@@ -74,74 +113,177 @@ $app->get('/users/new', function ($request, $response) {
 
 $app->post('/users', function ($request, $response) use ($router) {
     $validator = new Validator();
-    $repo = new Repository();
+    $repo = new Repository('users');
 
     $user = $request->getParsedBodyParam('user');
+
+// $insertID = $json->insert(array('column' => 'value'));
+
+  //$insertUser = $repo->add();
+ // $insertName = $json->insert(array('name' => 'ADA', 'email' => 'email', 'id' => '3'));
+
+  //$insertUser = $repo->add(array($name => 'name', $email => 'email', $id => 'id'));
+  //$insertName2 = $json->add(array('name' => 'UUU', 'email' => 'u@rt', 'id' => '1'));
+
+   //$user = $repo->add(array('id' => $id, 'nickname' => $nickname, 'email' => $email));
+
+//     $insertUser = $repo->add(['nickname' => $nickname]);
+   
+
+   // $insertID = $repo->add(array('column' => 'value'));
+    
+  // var_dump($insertUser2); die;
+  // var_dump($insertUser2); die;
+
+
+    // $user = $request->getParsedBodyParam('user');
     $errors = $validator->validate($user);
 
     if (empty($errors)) {
-        $repo->add($user);
+
+        // $repo->add($user);
+        //$user = $repo->add(['id' => $id]);
+     //  $usersData = $repo->add(array('id' => $user['id'], 'nickname' => $user['nickname'], 'email' => $user['email']));
+    // $usersData = $repo->add(array('name' => 'UUU', 'email' => 'u@rt', 'id' => '1'));  
+    // var_dump($insertName2);die;
+
+      // $usersData = $repo->add([]); //????
+
+    //   $id = $user['id'];
+    //   $nickname = $user['nickname'];
+    //   $email = $user['email'];
+      $data = ['id' => $user['id'], 'nickname' => $user['nickname'], 'email' => $user['email']];
+   // var_dump($data);die;
+
+    //  $insert = $repo->add(array('nickname' => $nickname, 'email' => $email, 'id' => $id));
+    $users = $repo->add($data);
+   //var_dump($users);die;
+
+    //  $insertName2 = $json->insert(array('name' => 'UUU', 'email' => 'u@rt', 'id' => '1'));
+
+     //  $repo->add(['nickname' => $nickname, 'email' => $email, 'id' => $id]);
+
+        // $insertID = $json->insert(array('column' => 'value'));
+
         $url = $router->urlFor('users');
         $this->get('flash')->addMessage('success', 'User has been added');
         return $response->withRedirect($url, 302);
     }
     $params = [
-        'user' => $user,
+        'users' => $users,
         'errors' => $errors
     ];
     return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
 });
 
 $app->get('/users/{id}', function ($request, $response, $args) {
-    $repo = new Repository();
+    $repo = new Repository('users');
     $id = $args['id'];
-    $user = $repo->get($id);
+
+    // var_dump($id);die;
+    
+    $users = $repo->select(['id' => $id]);
+   //$users = $repo->select();
+    //$users = $repo->select([]);
+   // var_dump($users);die;
+
+    // $users = $repo->select(array('id' => '6067288632d29'));
+    // $users = $repo->select($users[$id]); //get
     $params = [
-        'user' => $user,
+        'users' => $users,
     ];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 })->setName('user');
 
+// $app->get('/users/{id}/edit', function ($request, $response, $args) {
+//     $repo = new Repository('users');
+//     $id = $args['id'];
+
+//     //$user = $repo->select($id); //get
+//     $user = $repo->select(['id' => $id]);
+
+//    // var_dump($user);die;
+
+//     $messages = $this->get('flash')->getMessages();
+//     $params = [
+//         'user' => $user,
+//         'userData' => $user,
+//         'errors' => [],
+//         'flash' => $messages
+//     ];
+//     return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+// })->setName('editUser');
+
 $app->get('/users/{id}/edit', function ($request, $response, $args) {
-    $repo = new Repository();
+    $repo = new Repository('users');
     $id = $args['id'];
-    $user = $repo->get($id);
-    $messages = $this->get('flash')->getMessages();
+    $user = $repo->select(['id' => $id]);
+  //  var_dump($user);die;
+
+  $messages = $this->get('flash')->getMessages();
+
+    // $id = $args['id'];
+    // $users = getData();
+    // $user = $users[$id];
+    
     $params = [
         'user' => $user,
-        'userData' => $user,
         'errors' => [],
         'flash' => $messages
     ];
     return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
-})->setName('editUser');
+});
+
 
 
 $app->patch('/users/{id}', function ($request, $response, array $args) use ($router) {
     $id = $args['id'];
-    $repo = new Repository();
-    $user = $repo->get($id);
-    $data = $request->getParsedBodyParam('user');
+    $repo = new Repository('users');
 
-    $validator = new Validator();
-    $errors = $validator->validate($data);
+    
+   $user = $repo->select(['id' => $id]);
 
-    if (count($errors) === 0) {
-        // Ручное копирование данных из формы в нашу сущность
-        $user->nickname = $data['nickname'];
-        $user->email = $data['email'];
-        $repo->save($user);
+   $data = $request->getParsedBodyParam('user');
+
+  
+
+  
+
+//    var_dump($data);
+//    var_dump($id);
+
+
+   $validator = new Validator();
+   $errors = $validator->validate($data);
+
+// if (is_null($data['id'])) {
+//     echo 1;
+//     $data['id'] = $id;
+// }
+
+var_dump($user);//die;
+
+foreach ($user as $key => $value) {
+   // $value['id'] = $data['id'];
+    $value['nickname'] = $data['nickname'];
+    $value['email'] = $data['email'];
+    $user = $repo->add1($value);
+}
+
+// $user = $repo->add1($user);
 
         $this->get('flash')->addMessage('success', 'User has been updated');
-        $url = $router->urlFor('user', ['id' => $user->id]);
+
+        $url = $router->urlFor('users');
+
         return $response->withRedirect($url);
-    }
+    
 
     $params = [
         'user' => $user,
-        'userData' => $data,
+        'data' => $data,
         'errors' => $errors,
-        'flash' => []
+        'flash' => [],
     ];
 
     $response = $response->withStatus(422);
@@ -149,10 +291,10 @@ $app->patch('/users/{id}', function ($request, $response, array $args) use ($rou
 });
 
 $app->delete('/users/{id}', function ($request, $response, array $args) use ($router) {
-    $repo = new Repository();
+    $repo = new Repository('users');
 
     $id = $args['id'];
-    $repo->destroy($id);
+    $repo->destroy(['id' => $id]);
 
     $url = $router->urlFor('users');
 
--- src/Repository.php
+++ src/Repository.php
@@ -4,58 +4,190 @@ namespace App;
 
 class Repository
 {
-    private const PATH = __DIR__ . '/../repo/users';
+    private $path;
 
-    public function readFile(string $filePath): string
+    public $data;
+
+    public function __construct($path)
     {
-        if (!file_exists(self::PATH)) {
-            throw new \Exception("The file file path does not exists.");
+        $this->path = $path . '.json';
+        if (!file_exists($this->path)) {
+            file_put_contents($this->path, '[]');
+            $this->data = [];
+        } else {
+            $this->readFile();
         }
+    }
+
+    private function readFile()
+    {
+        $this->data = json_decode(file_get_contents($this->path), true);
+    }
+
+    public function save()
+    {
+        file_put_contents($this->path, json_encode($this->data));
+    }
+
+    public function add(array $row = [])
+    {
+        if (!array_key_exists('id', $row)) {
+            $row['id'] = uniqid();
+            while (count($this->select(['id' => $row['id']])) > 0) {
+                $row['id'] = uniqid();
+            }
+        } 
+
+        $this->data[] = $row;
+        $this->save();
+        return $row;
+    }
+
+    public function add1(array $row = [])
+    {
+       $oldData = $this->select(['id' => $row['id']]);
+    //    var_dump($oldData);
+
+    //    var_dump($row); //die;
+
+       $result = [];
+       foreach ($oldData as $key => $value) {
+          foreach ($value as $item) {
+        $oldData[$key] = $row;
+        // die;
+          }
+
+          //var_dump($oldData);
+     }
+
+    // var_dump($oldData);die;
+
+     
+
+
+      // $newData[] = $row;
+    //   var_dump($this->data);
+    //     var_dump($row);
+    //     $this->data[] = $row;
+    //     var_dump($this->data);
+    //     die;
+
+      // var_dump($newData);
+
+     $this->data[] = $oldData;
+     var_dump($this->data);die;
+      $this->save();
+        return $oldData;
+    }
+
+    public function select(array $whereEquals = [])
+    {
+        $results = [];
+        $this->isCheckRow(
+            $whereEquals,
+            function ($row, $index) use (&$results) {
+                $results[] = $row;
+            }
+        );
+        return $results;
+    }
+
+
+    public function destroy(array $whereEquals = [])
+    {
+        $toDelete = [];
+        $this->isCheckRow(
+            $whereEquals,
+            function ($row, $index) use (&$toDelete) {
+                $toDelete[] = $index;
+            }
+        );
+        //loop backwords to avoid indeces going out of sync.
+        for ($i = count($toDelete) - 1; $i >= 0; $i -= 1) {
+            unset($this->data[$toDelete[$i]]);
+        }
+        $this->data = $this->reIndex($this->data);
+        $this->save();
+    }
+
+    private function isCheckRow(array $whereEquals, $callback)
+    {
+        for ($i = 0; $i < count($this->data); $i += 1) {
+            $row = $this->data[$i];
+            $isMatch = true;
+            foreach ($whereEquals as $column => $value) {
+                if (!array_key_exists($column, $row) || $row[$column] != $value) {
+                    $isMatch = false;
+                }
+            }
+            if ($isMatch) {
+                $callback($row, $i);
+            }
+        }
+    }
+
+    // Переиндексировать массив
+    private function reIndex(array $array)
+    {
+        $reIndex = [];
+        foreach ($array as $value) {
+            $reIndex[] = $value;
+        }
+        return $reIndex;
+    }
+
+    // private const PATH = __DIR__ . '/../repo/users';
+
+    // public function readFile(string $filePath): string
+    // {
+    //     if (!file_exists(self::PATH)) {
+    //         throw new \Exception("The file file path does not exists.");
+    //     }
         
-        return (string) file_get_contents(self::PATH);
-    }
+    //     return (string) file_get_contents(self::PATH);
+    // }
 
 
-    public function encodeData($data)
-    {
-        $formattedData = "\n" . json_encode($data);
-        file_put_contents(self::PATH, $formattedData, FILE_APPEND);   
-    }
+    // public function encodeData($data)
+    // {
+    //     $formattedData = "\n" . json_encode($data);
+    //     file_put_contents(self::PATH, $formattedData, FILE_APPEND);   
+    // }
 
-    public function add(array $data)
-    {
-        $id = uniqid();
-        $data['id'] = $id;
-        $this->encodeData($data);
-    }
+    // public function add(array $data)
+    // {
+    //     $id = uniqid();
+    //     $data['id'] = $id;
+    //     $this->encodeData($data);
+    // }
     
-    public function all()
-    {
-        $json = $this->readFile(self::PATH);
-        $dataStrings = explode("\n", $json);
-        $data = array_map(fn ($string) => json_decode($string), $dataStrings);
-        return $data;
-    }
+    // public function all()
+    // {
+    //     $json = $this->readFile(self::PATH);
+    //     $dataStrings = explode("\n", $json);
+    //     $data = array_map(fn ($string) => json_decode($string), $dataStrings);
+    //     return $data;
+    // }
 
-    public function get($id)
-    {
-        $allData = $this->all();
-        $data = collect($allData)->firstWhere('id', $id);
-        return $data;
-    }
+    // public function get($id)
+    // {
+    //     $allData = $this->all();
+    //     $data = collect($allData)->firstWhere('id', $id);
+    //     return $data;
+    // }
 
-    public function destroy(string $id)
-    {
-        unset($id);
-    }
+    // public function destroy(string $id)
+    // {
+    //     unset($id);
+    // }
 
-    public function save($data)
-    {
-        $id = $data->id;
-        $allData = $this->all();
-        $currentData = $this->get($id);
-        $indexOfData = array_search($currentData, $allData);
-        $allData[$indexOfData] = $data;
-        $out = array_map(fn ($item) => $this->encodeData($item), $allData);
-    }
+    // public function save($data)
+    // {
+    //     $id = $data->id;
+    //     $allData = $this->all();
+    //     $currentData = $this->get($id);
+    //     $indexOfData = array_search($currentData, $allData);
+    //     $allData[$indexOfData] = $data;
+    //     $out = array_map(fn ($item) => $this->encodeData($item), $allData);
+    // }
 }
